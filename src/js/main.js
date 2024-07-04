803b560422fdd0099ded03c0739ead0fdd3b8b7f
lobosilencioso <105953274+lobosilencioso@users.noreply.github.com>
2024-07-04 19:56:21 -0300

    g

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(docment).ready(function () {
     $('.btn-celular').on('click', function() {
         $('navcelular').toggleClass('ativo');
         $('.btn-celular').find('i').toggleClass('fa-x');
